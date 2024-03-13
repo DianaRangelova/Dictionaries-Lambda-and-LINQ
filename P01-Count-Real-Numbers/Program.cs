d04ec4b67b06ff11ef64863aedbc8b79faaa3a7d
DianaRangelova <diana_kirilova@mail.bg>
2024-03-13 22:22:57 +0200

    Dictionaries, Lambda and LINQ in C#

---
+++ P01-Count-Real-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿List<double> numbers = Console.ReadLine()
+                               .Split()
+                               .Select(double.Parse)
+                               .ToList();
+
+SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if(counts.ContainsKey(number))
+    {
+        counts[number]++;
+    }    
+    else
+    {
+        counts.Add(number, 1);
+    }
+}
+
+foreach (KeyValuePair<double, int> element in counts)
+{
+    Console.WriteLine($"{element.Key} -> {element.Value}");
+}
---
+++ P03-Word-Synonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+Dictionary<string, string> synonymsWords = new Dictionary<string, string>();
+
+for (int i = 0; i < count; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (synonymsWords.ContainsKey(word))
+    {
+        synonymsWords[word] += ", " + synonym;
+    }
+    else
+    {
+        synonymsWords.Add(word, synonym);
+    }
+}
+
+foreach (var wordPair in synonymsWords)
+{
+    Console.WriteLine($"{wordPair.Key} - {wordPair.Value}");
+}
